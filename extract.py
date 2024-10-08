24b3751eeeb22488a58ad706d85afff5d46a9075
alexandre ROUZIERE <125266802+alexandre25091991@users.noreply.github.com>
2024-10-08 21:18:20 +0200

    commit

--- extract.py
+++ extract.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/extract", methods=['POST'])
+def extract():
+    address = request.form['address']
+    address_json = json.dumps({"address": address})
+    print(address_json)
+    return "Adresse traitée"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
